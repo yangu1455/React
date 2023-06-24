978ca13aa5d1592cc858d6cd8bffc4e897e719c4
yangu1455 <kunja266@hanmail.net>
2023-06-24 18:36:15 +0900

    중괄호&삼항연산자까지 활용해 조건에 따라 다른 렌더링까지

--- toy-project/bite_size_prac/src/App.js
+++ toy-project/bite_size_prac/src/App.js
@@ -13,11 +13,21 @@ function App() {
     },
   };
 
+  const func = () => {
+    return 'true'
+  }
+
+  const number = 5;
+
   return (
     <div className="App">
       <MyHeader/>
-      <h2>{name} 최고...</h2>
+      <h2>{name} 최고의 동그라미... {100+15} {true}{func()} {"love"}</h2>
       <b style={im_style.im_text} id="bold_text">overdrive 앨범 맛집</b>
+      <br/>
+      <b style={im_style.im_text} id="bold_text">
+        {number}는? {number % 2 === 0 ? '짝수' : '홀수'}
+      </b>
       <Counter />
       <MyFooter/>
     </div>
