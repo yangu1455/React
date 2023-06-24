5dfbc80ea3565ad081181937bf5a97f96333744f
yangu1455 <kunja266@hanmail.net>
2023-06-25 00:33:45 +0900

    Counter를 만들어서 state 배우기

--- toy-project/bite_size_prac/src/Counter.js
+++ toy-project/bite_size_prac/src/Counter.js
@@ -1,9 +1,42 @@
+import React, {useState} from "react";
+
 const Counter = () => {
+  // 0에서 출발
+  // 1씩 증가하고
+  // 1씩 감소하는
+  // count 상태
+
+  console.log("counter 호출!");
+
+  const [count, setCount] = useState(0);
+
+  const onIncrease = () => {
+    setCount(count + 1);
+  }
+
+  const onDecrease = () => {
+    setCount(count - 1);
+  }
+
+  const [count2, setCount2] = useState(0);
+
+  const onIncrease2 = () => {
+    setCount2(count2 + 1);
+  }
+
+  const onDecrease2 = () => {
+    setCount2(count2 - 1);
+  }
+  
   return (
     <div className="Counter">
-      <h1>0</h1>
-      <button>+</button>
-      <button>-</button>
+      <h1>{count}</h1>
+      <button onClick={onIncrease}>+</button>
+      <button onClick={onDecrease}>-</button>
+
+      <h1>{count2}</h1>
+      <button onClick={onIncrease2}>+</button>
+      <button onClick={onDecrease2}>-</button>
     </div>
   );
 };
