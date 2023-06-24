93b079953177349eb05ba375d4dfcaa0c616051e
yangu1455 <kunja266@hanmail.net>
2023-06-24 18:28:50 +0900

    style 적용하기_2 app 함수내에서 점표기법 사용한 style 적용하기

--- toy-project/bite_size_prac/src/App.js
+++ toy-project/bite_size_prac/src/App.js
@@ -7,11 +7,17 @@ import MyHeader from './MyHeader';
 function App() {
   let name = '채형원';
 
+  const style = {
+    im_text: {
+      color: "blueviolet",
+    },
+  };
+
   return (
     <div className="App">
       <MyHeader/>
       <h2>{name} 최고...</h2>
-      <b id="bold_text">overdrive 앨범 맛집</b>
+      <b style={style.im_text} id="bold_text">overdrive 앨범 맛집</b>
       <Counter />
       <MyFooter/>
     </div>
