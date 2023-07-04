69d3f5dc242595a0cebab84dafa120bd32681d98
yangu1455 <kunja266@hanmail.net>
2023-07-04 13:42:58 +0900

    10. peerDeps 까지

--- toy-project/cdd-storybook-prac/rollup.config.js
+++ toy-project/cdd-storybook-prac/rollup.config.js
@@ -1,8 +1,21 @@
+// ESModule aka ESM
+import esbuild from 'rollup-plugin-esbuild';
+
+// CommonJS aka CJS
+const esbuild2 = require('rollup-plugin-esbuild')
+
 // rollup.config.js
 export default {
 	input: 'src/index.js',
 	output: {
 		file: 'bundle.js',
 		format: 'cjs'
-	}
+	},
+  plugins: [
+    esbuild({
+      include: /\.jsx?$/,
+      minify: process.env.NODE_ENV === 'production',
+      jsx: 'automatic',
+    }),
+  ],
 };
