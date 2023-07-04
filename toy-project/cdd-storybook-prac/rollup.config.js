ee0d2b022502b7e4b0f14c5d7046887c5f1a6f9a
yangu1455 <kunja266@hanmail.net>
2023-07-04 14:46:26 +0900

    11. ESM, CJS output 설정 변경

--- toy-project/cdd-storybook-prac/rollup.config.js
+++ toy-project/cdd-storybook-prac/rollup.config.js
@@ -1,13 +1,20 @@
 import esbuild from 'rollup-plugin-esbuild';
 import PeerExternal from 'rollup-plugin-peer-deps-external';
+import pkg from './package.json';
 
 // rollup.config.js
 export default {
 	input: 'src/index.js',
-	output: {
-		file: 'bundle.js',
-		format: 'cjs'
-	},
+	output: [
+    {
+      file: 'bundle.js',
+      format: 'esm'
+    },
+    {
+      file: 'bundle.js',
+      format: 'cjs'
+    },
+  ],
   plugins: [
     esbuild({
       include: /\.jsx?$/,
