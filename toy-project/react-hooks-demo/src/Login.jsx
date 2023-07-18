a832a154f73ad5c82303d32674b0872693d20cd2
yangu1455 <kunja266@hanmail.net>
2023-07-18 18:15:58 +0900

    1차로 css 파일 만들고 적용

--- toy-project/react-hooks-demo/src/Login.jsx
+++ toy-project/react-hooks-demo/src/Login.jsx
@@ -12,15 +12,21 @@ export default function Login() {
       <div className="contentWrap">
         <div className="inputTitle">이메일 주소</div>
         <div className="inputWrap">
-          <input className="input"/>
+          <input 
+            className="input"
+            placeholder="test@gmail.com" 
+          />
         </div>
         <div className="errorMessageWrap">
           올바른 이메일을 입력해주세요.
         </div>
 
-        <div className="inputTitle">비밀번호</div>
+        <div style={{ marginTop: "26px" }} className="inputTitle">비밀번호</div>
         <div className="inputWrap">
-          <input className="input"/>
+          <input 
+            className="input"
+            placeholder="영문, 숫자, 특수문자 포함 8자 이상"
+          />
         </div>
         <div className="errorMessageWrap">
           영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.
@@ -28,7 +34,7 @@ export default function Login() {
       </div>
 
       <div>
-        <button>
+        <button disabled={true} className="bottomButton">
           확인
         </button>
       </div>
