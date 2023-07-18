ad7fd6f5d888e6965d308387f7eb89d36d49ffff
yangu1455 <kunja266@hanmail.net>
2023-07-19 01:17:04 +0900

    이메일과 비밀번호 input

--- toy-project/react-hooks-demo/src/Login.jsx
+++ toy-project/react-hooks-demo/src/Login.jsx
@@ -1,6 +1,35 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [pw, setPw] = useState("");
+
+  const [emailValid, setEmailValid] = useState(false);
+  const [pwValid, setPwValid] = useState(false);
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    const regex = 
+      /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+    if(regex.test(email)) {
+      setEmailValid(true);
+    } else {
+      setEmailValid(false);
+    }
+  }
+
+  const handlePassword = (e) => {
+    setPw(e.target.value);
+    const regex = 
+      /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
+    if(regex.test(pw)) {
+      setPwValid(true);
+    } else {
+      setPwValid(false);
+    }
+  }
+
   return (
     <div className='page'>
       <div className="titleWrap">
@@ -13,23 +42,37 @@ export default function Login() {
         <div className="inputTitle">이메일 주소</div>
         <div className="inputWrap">
           <input 
+            type='text'
             className="input"
             placeholder="test@gmail.com" 
+            value={email}
+            onChange={handleEmail}
           />
         </div>
         <div className="errorMessageWrap">
-          올바른 이메일을 입력해주세요.
+          {
+            !emailValid && email.length > 0 && (
+              <div>올바른 이메일을 입력해주세요.</div>
+            )
+          }
         </div>
 
         <div style={{ marginTop: "26px" }} className="inputTitle">비밀번호</div>
         <div className="inputWrap">
           <input 
+            type="password"
             className="input"
             placeholder="영문, 숫자, 특수문자 포함 8자 이상"
+            value={pw}
+            onChange={handlePassword}
           />
         </div>
         <div className="errorMessageWrap">
-          영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.
+          {
+            !pwValid && pw.length > 0 && (
+              <div>영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.</div>
+            )
+          }
         </div>
       </div>
 
