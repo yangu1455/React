c0f11a0e260176da9147d36e8c72011f2148ff7d
yangu1455 <kunja266@hanmail.net>
2023-07-29 23:43:43 +0900

    8강 듣는중

--- toy-project/inv-first-proj/src/components/List.jsx
+++ toy-project/inv-first-proj/src/components/List.jsx
@@ -3,12 +3,33 @@ import React from 'react'
 function List() {
 
   const [newName, setNewName] = useState("");
+  const [personList, setPersonList] = useState([]);
+
+  const addPerson = () => {
+    // input 창을 비워주기
+    setNewName('');
+  
+    // input 창에 입력한 값을 배열에 추가하기
+    setPersonList([
+      ...personList,  
+      {
+        id: nextId++,
+        name: newName,
+      },
+    ]);
+  }
+
 
   return (
     <>
       <h1>떠든 사람:</h1>
       <input value={newName} onChange={(e) => setNewName(e.target.value)}/>
-      <button>추가</button>
+      <button onClick={addPerson}>추가</button>
+      <ul>
+        {personList.map((person) => (
+          <li key={person.id}>{person.name}</li>
+        ))}
+      </ul>
     </>
   )
 }
