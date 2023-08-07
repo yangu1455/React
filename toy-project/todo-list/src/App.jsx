736462b5ea454d98b2c692a40ecd2bed26e70fd9
yangu1455 <kunja266@hanmail.net>
2023-08-08 00:12:33 +0900

    grocery bud add 기능까지

--- toy-project/todo-list/src/App.jsx
+++ toy-project/todo-list/src/App.jsx
@@ -14,7 +14,20 @@ function App() {
   const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('hello')
+    if(!name){
+      // display alert
+  
+    }
+    else if(name && isEditing){
+      // deal with edit
+
+    }
+    else{
+      // show alert
+      const newItem = { id: new Date().getTime().toString(), title:name };
+      setList([...list, newItem]);
+      setName('')
+    }
   }
 
 
@@ -30,10 +43,12 @@ function App() {
           </button>
         </div>
       </form>
-      <div className="grocery-container">
-        <List/>
-        <button className='clear-btn'>clear items</button>
-      </div>
+      {list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className='clear-btn'>clear items</button>
+        </div>
+      )}
     </section>
   )
 }
--- toy-project/todo-list/src/List.jsx
+++ toy-project/todo-list/src/List.jsx
@@ -1,11 +1,28 @@
 import react from "react";
-// import { FaEdit, FaTrash } from 'react-icons/fa';
+import { FaEdit, FaTrash } from 'react-icons/fa';
 
-const List = () => {
+const List = ({items}) => {
   return (
-    <>
-      <h2>List component</h2>
-    </>
+    <div className="grocery-list">
+      {items.map((item)=>{
+        const {id, title} = item;
+
+        return (
+          <article key={id} className="grocery-item">
+            <p className="title">{title}</p>
+            <div className="btn-container">
+              <button type="button" className="edit-btn">
+                <FaEdit />
+              </button>
+              <button type="button" className="delete-btn">
+                <FaTrash />
+              </button>
+            </div>
+          </article>
+        )  
+      })}
+      
+    </div>
   )
 }
 
