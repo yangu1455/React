5483ae6644ff3de97e4c3688d4e5f205b1c13b94
yangu1455 <kunja266@hanmail.net>
2023-08-09 14:50:43 +0900

    navbar useRef

--- toy-project/todo-list-with-nav/src/Navbar.jsx
+++ toy-project/todo-list-with-nav/src/Navbar.jsx
@@ -6,6 +6,19 @@ import { links, social } from "./data";
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null)
+  const linksRef = useRef(null)
+
+  useEffect(()=>{
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    console.log(linksContainerRef.current.getBoundingClientRect())
+    if(showLinks){
+      linksContainerRef.current.style.height = `${linksHeight}px`
+    }
+    else{
+      linksContainerRef.current.style.height = '0px'
+    }
+  },[showLinks])
 
   return (
     <nav>
@@ -15,8 +28,8 @@ const Navbar = () => {
             <FaBars/>
           </button>
         </div>
-        <div className={`${showLinks ? "links-container show-container" : "links-container"}`}>
-          <ul className="links">
+        <div className="links-container" ref={linksContainerRef} >
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { id, url, text, content } = link;
               return (
