23347fb4ae50616317d8f5d940fcf4d4b93a8a85
yangu1455 <kunja266@hanmail.net>
2023-09-05 18:33:18 +0900

    간단한 일기장 - React에서 사용자 입력 처리하기

---
+++ toy-project/simple-diary/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import DiaryEditor from "./DiaryEditor";
+
+function App() {
+
+  return (
+    <>
+      <h2>일기장</h2>
+      <DiaryEditor />
+    </>
+  )
+}
+
+export default App
