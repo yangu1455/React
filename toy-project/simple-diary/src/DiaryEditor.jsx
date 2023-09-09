0b31abba71fe9ec085c2808174fb7bc1fee884e7
yangu1455 <kunja266@hanmail.net>
2023-09-09 17:07:22 +0900

    React에서 DOM 조작하기 - useRef

--- toy-project/simple-diary/src/DiaryEditor.jsx
+++ toy-project/simple-diary/src/DiaryEditor.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 const DiaryEditor = () => {
+  const authorInput = useRef();
+  const contentInput = useRef();
+
   const [state, setState] = useState({
     author: "",
     content: "",
@@ -15,8 +18,17 @@ const DiaryEditor = () => {
   }
   
   const handleSubmit = () => {
-    console.log(state);
-    alert("저장 성공");
+    if(state.author.length < 1) {
+      authorInput.current.focus();
+      return;
+    }
+
+    if(state.content.length < 5) {
+      contentInput.current.focus();
+      return;
+    }
+
+    alert("저장 성공!");
   }
 
   return (
@@ -24,6 +36,7 @@ const DiaryEditor = () => {
       <h2>오늘의 일기</h2>
       <div>
         <input 
+          ref={authorInput}
           name='author'
           value={state.author} 
           type="text" 
@@ -32,6 +45,7 @@ const DiaryEditor = () => {
       </div>
       <div>
         <textarea 
+          ref={contentInput}
           name='content'
           value={state.content} 
           onChange={handleChangeState}
